b5a8a186a05690d08246414e02203c67cb35f8ff
Rafibarides <rafibaridesmusic@gmail.com>
2023-10-08 13:15:22 -0400

    front

--- randaff.js
+++ randaff.js
@@ -1,15 +1,17 @@
-let one = "You've got this"
-let two = "Your efforts are valued"
-let three = "You make a difference"
-let four = "You're stronger than you think"
-let five = "Your potential is limitless"
-let six = "You are deserving of love"
+let one = "You've got this";
+let two = "Your efforts are valued";
+let three = "You make a difference";
+let four = "You're stronger than you think";
+let five = "Your potential is limitless";
+let six = "You are deserving of love";
 
-const affirmations = [one,two,three,four,five,six]
+const affirmations = [one, two, three, four, five, six];
 
-const affirm = () =>{ 
-const rand = Math.floor(Math.random() * affirmations.length)
-return affirmations[rand]
-}
+const affirm = () => {
+    const rand = Math.floor(Math.random() * affirmations.length);
+    return affirmations[rand];
+};
 
-console.log(affirm())
+document.getElementById("affirmation-button").addEventListener("click", () => {
+    document.getElementById("affirmation-text").innerText = affirm();
+});
